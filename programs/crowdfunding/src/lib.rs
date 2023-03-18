b71f544da4cb220c4bbb946b54de6eaed9c17004
kanbaknurullah <kanbaknurullah@gmail.com>
2023-03-18 21:58:09 +0300

    deployed to devnet

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DS2TdQSe8e9kJtHANfYEFz1BErzdpDXV2NfMfa8BqoRE");
 
 #[program]
 pub mod crowdfunding {

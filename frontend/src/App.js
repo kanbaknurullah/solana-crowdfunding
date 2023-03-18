5f356a8a4e1996f43a409ad69a8e4b8602e662cf
kanbaknurullah <kanbaknurullah@gmail.com>
2023-03-18 23:20:53 +0300

    campaign creation is implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import {useEffect, useState} from 'react';
+import idl from "./idl.json";
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import {Program, AnchorProvider, web3, utils} from '@project-serum/anchor';
 import './App.css';
+import {Buffer} from 'buffer';
+window.Buffer = Buffer;
+
+const programID = new PublicKey(idl.metadata.address);
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: "processed",
+};
+const {SystemProgram} = web3;
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment);
+    return provider;
+  }
   const checkIfTheWalletIsConnected = async() => {
     try {
       const {solana} = window;
@@ -29,8 +46,32 @@ const App = () => {
     }
   };
 
+  const createCampaign = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const [campaign] = await PublicKey.findProgramAddress([
+        utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
+        provider.wallet.publicKey.toBuffer()
+      ], program.programId);
+      await program.rpc.create('campaign name', 'campaign description', {
+        accounts: {
+          campaign: campaign,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId
+        }
+      });
+      console.log("Created a new campaign w/ address: ", campaign.toString());
+    } catch (error) {
+      console.log("Error creating campaign account: ", error)
+    }
+  }
+
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet}>Connect to Wallet</button>
+  )
+  const renderConnectedContainer = () => (
+    <button onClick={createCampaign}>Create a campaign...</button>
   )
   useEffect(() => {
     const onLoad = async() => {
@@ -41,7 +82,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className='App'>{!walletAddress && renderNotConnectedContainer()}</div>
+    <div className='App'>{!walletAddress ? renderNotConnectedContainer() : renderConnectedContainer()}</div>
   )
 }
 

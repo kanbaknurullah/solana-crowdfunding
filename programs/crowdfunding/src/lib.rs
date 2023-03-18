636c63e5712f872307124fac1c190f46f22c6890
kanbaknurullah <kanbaknurullah@gmail.com>
2023-03-18 18:06:23 +0300

    campaign create function is implemented

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -5,11 +5,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod crowdfunding {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    // name is the campaign name, description is the campaign description
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
+        Ok(());
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
